4da2abc220ea3635a50a88890afc2c2532c28a31
Athlon007 <aathlon@outlook.com>
2023-03-27 13:15:52 +0200

    Update OpinionsList.vue

--- src/components/AdminComponents/OpinionsList.vue
+++ src/components/AdminComponents/OpinionsList.vue
@@ -11,14 +11,27 @@
         <div class="row">
             <div class="col-6 card">
                 <h4>Opinions</h4>
-                <div class="card" v-for="opinion in opinions" :key="opinion.id">
+                <div class="card" v-for="opinion in opinions" :key="opinion.id" @click="this.editOpinion(opinion)">
                     <h3>{{ opinion.title }}</h3>
                     <p>{{ opinion.content }}</p>
                 </div>
             </div>
             <div class="col-6 card">
                 <h4>Stats</h4>
-                <p>fuck</p>
+                <form class="editor">
+                    <label for="title">Title</label>
+                    <input type="text" id="title" class="form-control" v-model="editedOpinion.title">
+                    <label for="content">Content</label>
+                    <textarea id="content" class="form-control" v-model="editedOpinion.content"></textarea>
+                    <button type="button" class="btn btn-success" @click="saveOpinion">Save</button>
+                    <button type="button" class="btn btn-danger" @click="deleteOpinion">Delete</button>
+                    <button type="button" class="btn btn-secondary" @click="clearEditor">Cancel</button>
+                </form>
+                <div v-for="report in editedOpinionReports" :key="report.id">
+                    <p><strong>Name: {{ report.name }}</strong></p>
+                    <p :class="{ warn: report.count > 0 }">Count: {{ report.count ?? 0 }}</p>
+                </div>
+                <button type="button" class="btn btn-success" @click="pardonOpinion">Pardon</button>
             </div>
         </div>
     </div>
@@ -34,7 +47,12 @@ export default {
             topics: [],
             selectedTopic: null,
             opinions: [],
-            editedOpinion: null
+            editedOpinion: {
+                id: null,
+                title: null,
+                content: null
+            },
+            editedOpinionReports: []
         }
     },
     methods: {
@@ -50,6 +68,53 @@ export default {
             try {
                 const response = await axios.get('/topics');
                 this.topics = response.data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        clearEditor() {
+            this.editedOpinion = {
+                id: null,
+                title: null,
+                content: null
+            }
+        },
+        async deleteOpinion() {
+            try {
+                await axios.delete('/opinions/' + this.editedOpinion.id);
+                this.getOpinions();
+                this.clearEditor();
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async saveOpinion() {
+            try {
+                await axios.put('/opinions/' + this.editedOpinion.id, this.editedOpinion);
+                this.getOpinions();
+                this.clearEditor();
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async editOpinion(opinion) {
+            this.editedOpinion = JSON.parse(JSON.stringify(opinion));
+            this.getOpinionReports();
+        },
+        async getOpinionReports() {
+            try {
+                const response = await axios.get('/opinions/' + this.editedOpinion.id + '/reports');
+                this.editedOpinionReports = response.data;
+
+                console.log(this.editedOpinionReports);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async pardonOpinion() {
+            try {
+                await axios.delete('/opinions/report/' + this.editedOpinion.id);
+                this.getOpinionReports();
             } catch (error) {
                 console.error(error);
             }
@@ -70,5 +135,18 @@ export default {
 
 .row {
     width: 100%;
+}
+
+.editor>input,
+textarea {
+    width: 100%;
+}
+
+.card {
+    flex-grow: 1;
+}
+
+.warn {
+    color: red;
 }
 </style>
